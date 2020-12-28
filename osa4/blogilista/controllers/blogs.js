ab747a4b1ebbc2917ea1a1bef6e7295b0835fe56
Jenni Kontturi <jeevko@utu.fi>
2020-12-28 16:09:29 +0200

    4.8 ja 4.10

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,24 +2,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-
-blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-      .catch(error => next(error))
+blogsRouter.get('/', async (request, response) => { 
+  const blogs = await Blog.find({})
+  response.json(blogs.map(blog => blog.toJSON()))
 })
 
+blogsRouter.post('/', async (request, response) => {
+    const blog = new Blog(request.body)
+
+    const result = await blog.save()
+    response.status(201).json(result)
+})
 
 module.exports = blogsRouter
